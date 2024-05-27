0adacc64ddba827f4b1e188dce0450273df73917
erick-ondiwa <erickochieng3860@gmail.com>
2024-05-27 22:37:39 +0300

    Third push to advanced js

--- to_do.js
+++ to_do.js
@@ -1,9 +1,21 @@
 const new_task = document.getElementById('addNewTask');
-new_task.style.display = 'block';
+
+const welcome_page = document.getElementById('welcome_page');
+
+const task_items = document.getElementById('display_task_items');
+task_items.addEventListener('click', () =>{
+  new_task.style.display = 'block';
+  welcome_page.style.display = 'none';
+})
+
+const today_tasks = document.getElementById('today_button');
+today_tasks.addEventListener('click', () =>{
+  display_list.style.display = 'block';
+  welcome_page.style.display = 'none';
+});
 
 
-const display_list = document.querySelector(".display_list");
-display_list.style.display = 'none';
+const display_list = document.getElementById('display_list');
 
 const alert_message = document.querySelector('.alertmessage');
 alert_message.style.display = 'none';
@@ -18,8 +30,6 @@ const addbutton = document.querySelector(".add_button");
       alert_message.style.display = 'block';
     } 
 });
-
-
 let deletebutton = document.createElement('button');
 deletebutton.addEventListener('click', function(){
   itemsDiv.removeChild(listItems);
@@ -29,7 +39,6 @@ function getTasks(){
   const tasks = localStorage.getItem('tasks');
   return tasks ? JSON.parse(tasks) : [];
 }
-
 function addTask(task){
   const tasks = getTasks();
   tasks.push(task);
@@ -39,14 +48,12 @@ function addTask(task){
 function saveTasks(tasks){
    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
-
 const view_button = document.getElementById('viewButton');
 view_button.addEventListener('click', () =>{
   display_list.style.display = 'block';
   new_task.style.display = 'none';
   alert_message.style.display = 'none';
 });
-
 function displayTasks(){
   const tasks = getTasks();
   const itemsDiv = document.createElement('ul');
@@ -60,13 +67,5 @@ function displayTasks(){
  });
 
 }
-  //  const currentdate = document.getElementById("currentdate");  localStorage.setItem('dueDate', JSON.stringify(currentdate.value));
-
-//   let dueDate = localStorage.getItem('dueDate');
-//   let taskDueDate = JSON.parse(dueDate);
-
-//   const todoname = document.getElementById("todoName").value;
-//   if(todoname){   
-//     getTask(); 
-//    
+    
 
